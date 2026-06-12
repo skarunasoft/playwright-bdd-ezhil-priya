7db1e30e7384ad0881662065df50ff4a46f94ffc
karunamoorthy-scooter <karunamoorthys@tnqtech.com>
2026-06-12 16:56:23 +0530

    added one more test method

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -34,3 +34,29 @@ test('Second First Playwright Test', async ({ page }) => {
     await expect(price).toContain('$29.99');
 
 });
+
+
+
+test('Third Playwright Test', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    // await page.pause();
+    await page.locator('#user-name').fill('standard_user');
+    await page.locator('#password').fill('secret_sauce');
+
+
+    const typedUsername = await page.locator('#user-name').inputValue();
+    const typedPassword = await page.locator('#password').inputValue();
+
+    await expect(typedUsername).toBe('standard_users');
+    await expect(typedPassword).toBe('secret_sauce');
+
+
+    await page.locator('#login-button').click();
+    await expect(page).toHaveTitle('Swag Labs');
+    await page.locator('#item_4_title_link > div').click();
+    const price = await page.locator('.inventory_details_price').textContent();
+    console.log(price);
+    //await page.puase(); ==> to open the Inspector tool
+    await expect(price).toContain('$29.99');
+
+});
