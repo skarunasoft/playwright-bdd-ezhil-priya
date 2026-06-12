c32e1fd229c2e640891967155e41ea1b51971340
karunamoorthy-scooter <karunamoorthys@tnqtech.com>
2026-06-12 16:31:05 +0530

    playwright frameword added

---
+++ cucumber.js
@@ -0,0 +1,20 @@
+module.exports = {
+  default: {
+    require: ['features/step-definitions/*.js', 'hooks/hooks.js'],
+    paths: ['features/*.feature'],
+    format: ['html:reports/cucumber-report.html',     
+      'json:reports/cucumber-report.json',
+       'allure-cucumberjs/reporter',
+       'rerun:@rerun.txt',
+       'progress',
+       '@cucumber/pretty-formatter',
+        'summary'
+    ],
+    dryRun: false,
+    parallel: 3,
+    retry: 1,
+    formatOptions: {
+      resultsDir: 'allure-results'
+    }
+  }
+};
